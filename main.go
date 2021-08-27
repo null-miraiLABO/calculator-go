a505e9e72e62a916c7a482906a0c3a15c2c5473e
null-miraiLABO <mizuki2002b@gmail.com>
2021-08-27 15:16:26 +0900

    Refactoring

--- main.go
+++ main.go
@@ -6,21 +6,36 @@ import (
 )
 
 func main() {
-	var total int
-	limit := 10
-	formula := make([]string, limit)
+	formula := set()
+	fmt.Println(formula)
 
+	// 入力チェック後に出力
+	if formula[0] == "" || formula[0] == "+" || formula[0] == "-" || formula[0] == "*" || formula[0] == "/" {
+		fmt.Println("先頭には数字を入力して下さい。")
+	} else {
+		total := calc(formula)
+		fmt.Printf("= %d \n", total)
+	}
+
+}
+
+// 式をセット
+func set() []string {
+	limit := 10
+	tmp := make([]string, limit)
 	for i := 0; i < limit; i++ {
-		fmt.Scanf("%s", &formula[i])
-		if formula[i] == "" {
+		fmt.Scanf("%s", &tmp[i])
+		if tmp[i] == "" {
 			break
 		}
 	}
+	return tmp
+}
 
-	fmt.Println(formula)
-
+// 計算する
+func calc(formula []string) int {
 	tmp, _ := strconv.Atoi(formula[0])
-	total = tmp
+	total := tmp
 
 	for i := 0; i < len(formula); i++ {
 
@@ -38,8 +53,6 @@ func main() {
 			tmp, _ = strconv.Atoi(formula[i+1])
 			total = total / tmp
 		}
-
 	}
-
-	fmt.Printf("= %d \n", total)
+	return total
 }
